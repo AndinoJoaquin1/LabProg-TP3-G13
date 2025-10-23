67768ad0ea24e33436d10668158eaef875e3e5d9
Joaquin Andino <andino.joaquin.ezequiel@gmail.com>
2025-10-23 00:22:39 -0300

    comentando cositas

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,7 @@ import 'dotenv/config';
 
 //toma la uri guardada en el ,env
 const uri = `${process.env.MONGO_URI}`;
+
 //inicia express
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());//habilitar solicitudes a todos los origenes
 
 //inicia la coneccion con mongo
 await mongoose.connect(uri).then(()=>console.log("ola"));
+
 app.use(express.json());//parsea el json provenientes de la solicitudes para colcarlo en la req.body
 
 app.use('/auth', authRouter);//rutas de auth
--- server/schemas/user.ts
+++ server/schemas/user.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 import type { User } from "../types";
 
+//crea un nuevo esquema User, 
 const userSchema = new mongoose.Schema<User>({
     nickname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 })
 
+//y luego crea el modelo a partir de ese esquema, crea un model
+//el model contiene las funciones para crear un objeto con los atributos definidos en el esquema
+//y todas las fuciones relacionadas con mongo (create,find,etc)
 const UserModel = mongoose.model<User>('User', userSchema);
 
 export default UserModel;
