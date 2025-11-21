42ea9b829966f113311c4f7e4c0d985a12f8a9a3
Joaquin Andino <andino.joaquin.ezequiel@gmail.com>
2025-11-21 19:08:15 -0300

    feat: se agrego la funcionalidad para servir archivos estaticos

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { authRouter } from './routes/auth';
+import { authRouter } from './routes/auth.routes';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
@@ -12,11 +12,19 @@ const app = express();
 
 app.use(cors());//habilitar solicitudes a todos los origenes
 
+
 //inicia la coneccion con mongo
 await mongoose.connect(uri).then(()=>console.log("ola"));
 
 app.use(express.json());//parsea el json provenientes de la solicitudes para colcarlo en la req.body
 
 app.use('/auth', authRouter);//rutas de auth
+
+//rutas para obtener archivos estaticos(html/css/js)
+//express.static es un middleware que empaqueta? (creo) los archivos estaticos indicados en el path y para servirlo a los hosts
+app.use('/',express.static("static/home"))
+app.use('/components',express.static("static/components"))
+app.use('/common',express.static("static/common"))
+app.use('/images',express.static("static/img"))
 
 app.listen(3000, () => { console.log("ola2") })
